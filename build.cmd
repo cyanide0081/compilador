bb7b8ab968d8e3f49700935b6751e293e3bd4ca1
cyanide0081 <cyanide0081@gmail.com>
2024-08-15 16:53:50 -0300

    second commit

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-clang -o compiler_win32.exe compiler_win32.c -std=c99 -Wall -Wextra -pedantic -luser32 -lcomctl32 -lgdi32
+clang -o compiler_win32.exe compiler_win32.c -std=c99 -Wall -Wextra -pedantic -luser32 -lcomctl32 -lgdi32 -lcomdlg32
