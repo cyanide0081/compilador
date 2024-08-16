2e70850930a31fb4e77128e5f5b6a2211a0de9ed
cyanide0081 <cyanide0081@gmail.com>
2024-08-16 16:42:57 -0300

    add a lot of stuff

--- build.cmd
+++ build.cmd
@@ -1,1 +1,7 @@
-clang -o compiler_win32.exe compiler_win32.c -std=c99 -Wall -Wextra -pedantic -luser32 -lcomctl32 -lgdi32 -lcomdlg32
+@rem build script (gcc/clang + win32-msvc)
+@echo off
+
+where /Q gcc && (set "CC=gcc")
+where /Q clang && (set "CC=clang")
+
+%CC% -o compiler_win32.exe compiler_win32.c -std=c99 -Wall -Wextra -pedantic -luser32 -lcomctl32 -lgdi32 -lcomdlg32 -g -gcodeview
