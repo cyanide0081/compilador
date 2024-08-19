067708343b9824ccad96aeda2d60904f12cebf76
cyanide0081 <cyanide0081@gmail.com>
2024-08-19 16:33:54 -0300

    implement all essential features

--- build.cmd
+++ build.cmd
@@ -4,4 +4,35 @@
 where /Q gcc && (set "CC=gcc")
 where /Q clang && (set "CC=clang")
 
-%CC% -o compiler_win32.exe compiler_win32.c -std=c99 -Wall -Wextra -pedantic -luser32 -lcomctl32 -lgdi32 -lcomdlg32 -g -gcodeview
+if "%CC%" equ "" (
+	echo ERROR: unable to fing gcc or clang installed
+	exit /b
+)
+
+set "FLAGS=-std=c99 -Wall -Wextra -pedantic"
+set "LFLAGS=-luser32 -lcomctl32 -lgdi32 -lcomdlg32 -luxtheme"
+if "%CC%" equ "clang" (
+	set "DFLAGS=-g -gcodeview"
+) else (
+	set "FLAGS=%FLAGS% -municode"
+	set "DFLAGS=-g"
+)
+if "%~1" equ "rel" (
+	set "MFLAGS=-DNDEBUG"
+) else (
+	set "MFLAGS=%DFLAGS%"
+)
+
+@echo on
+%CC% -o compiler_win32.exe compiler_win32.c %FLAGS% %MFLAGS% %LFLAGS%
+@echo off
+
+set "run="
+if "%~1" equ "run" (
+	set "run=true"
+) else if "%~2" equ "run" (
+	set "run=true"
+)
+if defined run (
+	start compiler_win32.exe
+)
