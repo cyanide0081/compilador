4b28aea0b1769e8d80de87b71fa2667e9b13ec51
cyanide0081 <cyanide0081@gmail.com>
2024-12-19 14:32:46 -0300

    give debug exe unique name

--- build.cmd
+++ build.cmd
@@ -8,17 +8,20 @@ where /q %CC% || (
 	exit /b
 )
 
+set "EXE_NAME=compiler_win32"
+
 set "FLAGS=-std=c99 -Wall -Wextra -pedantic"
 set "LFLAGS=-luser32 -lcomctl32 -lgdi32 -lcomdlg32 -luxtheme -lshlwapi"
 set "FLAGS=%FLAGS% -municode -Wl,-subsystem,windows"
 if "%~1" == "debug" (
 	set "MFLAGS=-g -gcodeview -O0 -Wl,--pdb="
+	set "EXE_NAME=%EXE_NAME%_debug"
 ) else (
 	set "MFLAGS=-DNDEBUG -O2"
 )
 
 @echo on
-%CC% -o compiler_win32.exe compiler_win32.c %FLAGS% %MFLAGS% %LFLAGS% || exit /b
+%CC% -o %EXE_NAME%.exe compiler_win32.c %FLAGS% %MFLAGS% %LFLAGS% || exit /b
 @echo off
 
 set "run="
